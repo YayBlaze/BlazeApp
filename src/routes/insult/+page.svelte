80335fb8c8e348ee4426f9653fabe81adf23348a
Blaze Annison <blaze@Blazes-MacBook-Air.local>
2023-11-01 07:38:35 -0700

    feat: added more insults, retorts, names. Attempted to make random choice work, still in progress.

--- src/routes/insult/+page.svelte
+++ src/routes/insult/+page.svelte
@@ -1,11 +1,55 @@
 <title>Insult Fight - blazeannison</title>
 
-<script>
+<script lang="ts">
     let insultlist = [
         "I've meet pigs better at fighting then you.",
         "My great deeds are talked about across the land!",
         "I'm the greatest fighter on the seas!",
+        "Out of breath already?",
+        "Your mother.",
+        "No one has ever faced me and lived!"
     ]
+    let retortlsit = [
+        "Glad to hear you're still in touch with your parents",
+        "Well, everyones loves a good joke.",
+        "Too bad you're facing me on dry land.",
+        "Only from laughing at you!",
+        "Your father",
+        "With that bad breath, I'm not suprised."
+    ]
+    let names = [
+        "Tate the Great",
+        "Rexydoodles",
+        "Sword Loard Joe",
+        "Mr. all good bob",
+        "The Magic Man",
+        "Dan the Library Man",
+        "Your mother",
+        "Blaze Annison Himselfe",
+        "Mr. Develepor"
+    ]
+    let current1:string[] = []
+    let current2:string[] = []
+    let current3:string[] = []
+    function resetrng() {
+        for (let i = 0; i < 4; i++) {
+           let idx = Math.floor(Math.random() * 8)
+           let insult = insultlist[idx]
+           let retort = retortlsit[idx]
+           if (i == 0) {
+            current1.push(insult, retort)
+           }
+           else if (i == 1) {
+            current2.push(insult, retort)
+           }
+           else if (i == 2) {
+            current3.push(insult, retort)
+           }
+        }
+        console.log(current1)
+        console.log(current2)
+        console.log(current3)
+    }
 </script>
 
 
@@ -17,6 +61,7 @@
 <body id="body">
     <h2>Welcome to</h2>
     <h1>Insult Fight Game</h1>
+    <button on:click={resetrng}>Button</button>
 </body>
 
 
